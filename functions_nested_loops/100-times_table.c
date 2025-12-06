cfaeac6fe9e190aace69b44db7881536b654d1be
Leyla <leyla.khaspolad@gmail.com>
2025-12-06 22:34:37 +0000

    Add task 12

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table, starting with 0
+ * @n: The number defining the table size (must be between 0 and 15)
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int row, col, product;
+
+	if (n < 0 || n > 15)
+		return;
+
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			product = row * col;
+
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else if (product < 100)
+				{
+					_putchar(' ');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else
+				{
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
