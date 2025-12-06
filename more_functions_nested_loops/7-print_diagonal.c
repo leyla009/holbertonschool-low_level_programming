b273ce997664a5b1a54f64e30a77987f151728ce
Leyla <leyla.khaspolad@gmail.com>
2025-12-06 23:40:05 +0000

    Add task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal line using '\'
+ * @n: Number of times to print the diagonal
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int line, space;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (line = 0; line < n; line++)
+	{
+		for (space = 0; space < line; space++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
