409b0ee4542e59aa4a3173b154992efe4898ed06
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 11:39:00 +0000

    Fix Task 10 issues

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+
 int main(void)
 {
-    int n;
-    int a[5];
-    int *p;
+  int n;
+  int a[5];
+  int *p;
 
-    a[0] = 0;
-    a[1] = 1;
-    a[2] = 2;
-    a[3] = 3;
-    a[4] = 4;
-
-    p = a;
-    /*
-     * write your line of code here
-     * - you are not allowed to code anything else than this line of code
-     */
-    *(p + 2) = 98;
-    printf("a[2] = %d\n", *(p + 2));
-
-    return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 2) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
