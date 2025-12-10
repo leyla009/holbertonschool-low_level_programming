09e0662412de65ddcff111301cc00554096ef531
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 11:07:31 +0000

    Fix task 10

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -7,7 +7,6 @@ int main(void)
   int *p;
   a[2] = 1024;
   p = &n;
-
   /*
    * write your line of code here...
    * Remember:
@@ -20,4 +19,5 @@ int main(void)
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
+
 
