e36940d33380de3adfed5aacd328072586bb2dfd
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 11:31:04 +0000

    Fix Task 10 related issues

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,24 +1,23 @@
- 1 #include <stdio.h>
- 2 int main(void)
- 3 {
- 4     int n;
- 5     int a[5];
- 6     int *p;
- 7     /*
- 8      * write your line of code here
- 9      * Remember
-10      * - you are not allowed to use a
-11      * - you are not allowed to modify p
-12      * - only one statement
-13      * - you are not allowed to code anything else than this line of code
-14      */
-15     *(p + 2) = 98;
-16     /*
-17      * You can add extra comments or blank lines if needed
-18      * to reach the total of 23 lines
-19      * no punctuation rules break
-20      */
-21
-22     return (0);
-23 }
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    int a[5];
+    int *p;
 
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 2;
+    a[3] = 3;
+    a[4] = 4;
+
+    p = a;
+    /*
+     * write your line of code here
+     * - you are not allowed to code anything else than this line of code
+     */
+    *(p + 2) = 98;
+    printf("a[2] = %d\n", *(p + 2));
+
+    return (0);
+}
