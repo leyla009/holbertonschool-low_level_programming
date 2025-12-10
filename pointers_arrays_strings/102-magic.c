6e73e21e5f0433e65ce3784edf2906e3fdb13908
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 09:57:32 +0000

    Fix 102-magic.c issues

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+
 int main(void)
 {
-    int a[5] = {0, 1, 2, 3, 4};
+    int a[5];
     int *p = a;
 
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 2;
+    a[3] = 3;
+    a[4] = 4;
+
+   
     /* This line changes a[2] without using 'a' or comma */
     *(p + 2) = 98;
 
-    /* Print a[2] without using a comma in the added line */
-    printf("a[2] = ");
-    printf("98");
-    printf("\n");
+
+    /* Print the new value of a[2] */
+    printf("a[2] = %d\n", *(p + 2));
 
     return (0);
 }
