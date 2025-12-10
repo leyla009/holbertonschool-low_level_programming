3a327c54366a8b85bf58918728d527120afacc26
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 08:31:19 +0000

    Add Task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - Concatenates at most n bytes of src to the end of dest
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Maximum number of bytes to use from src
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	/* Find the end of dest */
+	while (dest[i] != '\0')
+		i++;
+
+	/* Append up to n bytes of src */
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	/* Add terminating null byte */
+	dest[i] = '\0';
+
+	return (dest);
+}
+
