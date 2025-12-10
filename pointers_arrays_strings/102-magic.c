31f1a4cd1b7f2b9200b76c37ed830fe4667cd8a0
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 10:07:35 +0000

    Fix 102-magic.c related issues

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-
 int main(void)
 {
+    int n;
     int a[5];
-    int *p = a;
+    int *p;
 
-    a[0] = 0;
-    a[1] = 1;
-    a[2] = 2;
-    a[3] = 3;
-    a[4] = 4;
-
-   
-    /* This line changes a[2] without using 'a' or comma */
+    /*
+     * write your line of code here...
+     * Remember:
+     * - you are not allowed to use a
+     * - you are not allowed to modify p
+     * - only one statement
+     * - you are not allowed to code anything else than this line of code
+     */
+    p = a;
     *(p + 2) = 98;
 
-
-    /* Print the new value of a[2] */
     printf("a[2] = %d\n", *(p + 2));
 
     return (0);
