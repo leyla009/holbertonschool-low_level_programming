14978bcdb40a2220474e00762b9ed01298e7efdd
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 09:04:26 +0000

    Fix issues at 102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
-
 int main(void)
 {
     int a[5] = {0, 1, 2, 3, 4};
     int *p = a;
+    *(p + 2) = 98
     printf("a[2] = %d\n", *(p + 2));
     
     return (0);
 }
-
-
-
-
-
-
-
-
-
-
