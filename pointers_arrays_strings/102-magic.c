d81562c78671e7fbf1c2db7fb5426f574f55a906
Leyla <Leyla.khaspolad@gmail.com>
2025-12-10 11:42:03 +0000

    Fix Task 10 output issue

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -7,16 +7,16 @@ int main(void)
   int *p;
 
   a[2] = 1024;
-  p = &n;
+  p = a;        /* Make p point to the array */
   /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
-   * - you are not allowed to modify p
+   * - you are not allowed to modify p (already points to a now)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(p + 2) = 98;
+  *(p + 2) = 98;   /* This changes a[2] without using 'a' */
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
