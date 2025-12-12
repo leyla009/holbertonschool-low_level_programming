22d311f45b2ee1cde3d166fd470a76483764db42
Leyla <Leyla.khaspolad@gmail.com>
2025-12-12 11:20:57 +0000

    Fix Task 11 issues

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -4,50 +4,44 @@
  * infinite_add - adds two numbers stored as strings
  * @n1: first number
  * @n2: second number
- * @r: buffer for the result
- * @size_r: buffer size
+ * @r: buffer where the result will be stored
+ * @size_r: size of buffer r
  *
- * Return: pointer to result, or 0 if result can't fit
+ * Return: pointer to result, or 0 if result cannot fit
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, j = 0, k, l, sum = 0, carry = 0;
+	int i = 0, j = 0, k = size_r - 2, carry = 0, sum;
 
+	/* find lengths */
 	while (n1[i] != '\0')
 		i++;
 	while (n2[j] != '\0')
-		j--;
+		j++;
 
-	i--;
-	j--;
+	i--, j--;
 
-	k = size_r - 1;
-	r[k] = '\0';
-	k--;
+	/* null terminate result */
+	if (size_r < 2)
+		return (0);
+	r[size_r - 1] = '\0';
 
+	/* add digits from right to left */
 	while (i >= 0 || j >= 0 || carry)
 	{
+		if (k < 0)
+			return (0);
+
 		sum = carry;
 		if (i >= 0)
 			sum += n1[i--] - '0';
 		if (j >= 0)
 			sum += n2[j--] - '0';
 
-		if (k < 0)
-			return (0);
-
-		r[k] = (sum % 10) + '0';
+		r[k--] = (sum % 10) + '0';
 		carry = sum / 10;
-		k--;
 	}
 
-	k++;
-
-	/* Shift result to beginning if needed */
-	for (l = 0; r[k + l] != '\0'; l++)
-		r[l] = r[k + l];
-	r[l] = '\0';
-
-	return (r);
+	/* shift result */
+	return (r + k + 1);
 }
-
