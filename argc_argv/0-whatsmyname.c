87e5fd995082cf6b17caec559adfbb814ba764f3
Leyla <Leyla.khaspolad@gmail.com>
2025-12-15 08:59:45 +0000

    Add 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of command line arguments (unused)
+ * @argv: array of command line argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* unused parameter */
+	printf("%s\n", argv[0]);
+	return (0);
+}                                                                                                         
---
+++ argc_argv/README.md
@@ -0,0 +1,17 @@
+# argc_argv
+
+This directory contains C programs that demonstrate the use of **command line arguments** (`argc` and `argv`) in C.
+
+## Files
+
+### 0-whatsmyname.c
+
+**Description:**  
+A program that prints its own name.  
+
+**Usage:**  
+```bash
+$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-whatsmyname.c -o mynameis
+$ ./mynameis
+./mynameis
+
