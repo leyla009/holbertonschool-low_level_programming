38b873a210d7e436f620640c9f9562b8ccbbb4a2
Leyla <Leyla.khaspolad@gmail.com>
2025-12-15 09:01:52 +0000

    Fix 0-whatsmyname.c issue

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc; /* unused parameter */
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
-}                                                                                                         
+}                                                           
