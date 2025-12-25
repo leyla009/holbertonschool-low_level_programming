d1c58486ba404b71aeb180fb2b2ed5ddda45821d
Leyla <Leyla.khaspolad@gmail.com>
2025-12-25 13:46:00 +0000

    Add 101-mul.c

---
+++ more_malloc_free/101-mul.c
@@ -0,0 +1,118 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _isdigit - checks if a string contains only digits
+ * @s: string to check
+ *
+ * Return: 1 if all digits, 0 otherwise
+ */
+int _isdigit(char *s)
+{
+	int i = 0;
+
+	if (s[0] == '\0')
+		return (0);
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
+ * _strlen - returns length of a string
+ * @s: string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * errors - prints Error and exits with status 98
+ */
+void errors(void)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	char *n1, *n2;
+	int len1, len2, len, i, j, carry, d1, d2;
+	int *result;
+
+	if (argc != 3)
+		errors();
+
+	n1 = argv[1];
+	n2 = argv[2];
+
+	if (!_isdigit(n1) || !_isdigit(n2))
+		errors();
+
+	len1 = _strlen(n1);
+	len2 = _strlen(n2);
+	len = len1 + len2;
+
+	result = malloc(sizeof(int) * len);
+	if (result == NULL)
+		return (1);
+
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		carry = 0;
+		d1 = n1[i] - '0';
+
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			d2 = n2[j] - '0';
+			carry += result[i + j + 1] + (d1 * d2);
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
+		}
+		result[i + j + 1] += carry;
+	}
+
+	i = 0;
+	while (i < len && result[i] == 0)
+		i++;
+
+	if (i == len)
+		_putchar('0');
+
+	for (; i < len; i++)
+		_putchar(result[i] + '0');
+
+	_putchar('\n');
+
+	free(result);
+	return (0);
+}
+
