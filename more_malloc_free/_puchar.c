8ec75a48b3f7e5f5d5118c3e67a6fd1b2d000ad0
Leyla <leyla.khaspolad@gmail.com>
2025-12-26 16:19:32 +0000

    Fix 101-mul.c

---
+++ more_malloc_free/_puchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
