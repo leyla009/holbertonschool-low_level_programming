7f358455af9bd2443d174f6fa2249ab15c6d6ccf
root <Leyla.khaspolad@gmail.com>
2025-12-29 09:57:20 +0000

    Fix 3-get_op_func.c issues

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -16,15 +16,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL)
+	{
+		if (s[0] == ops[i].op[0] && s[1] == '\0')
+			return (ops[i].f);
 		i++;
-
-	if (ops[i].op != NULL && *(ops[i].op) == *s && *(ops[i].op + 1) == '\0')
-		return (ops[i].f);
-
+	}
 	return (NULL);
 }
 
