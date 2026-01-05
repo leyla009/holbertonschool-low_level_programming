08191b52a9a43dcaee934057746cbf6d55f8b099
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 11:01:18 +0000

    Fix 3-print_all.c issues

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,43 +3,78 @@
 #include <stdarg.h>
 
 /**
- * print_all - Prints anything based on a format string
- * @format: List of argument types
- *
- * Return: Nothing
+ * print_char - prints a char
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str)
+		printf("%s", str);
+	if (!str)
+		printf("(nil)");
+}
+
+/**
+ * print_all - prints anything
+ * @format: format string
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *str;
+	unsigned int i = 0, j;
 	char *sep = "";
+
+	struct printer
+	{
+		char type;
+		void (*f)(va_list);
+	} printers[] = {
+		{'c', print_char},
+		{'i', print_int},
+		{'f', print_float},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		while (format[i] == 'c' || format[i] == 'i' ||
-		       format[i] == 'f' || format[i] == 's')
+		j = 0;
+		while (printers[j].type)
 		{
-			printf("%s", sep);
-
-			if (format[i] == 'c')
-				printf("%c", va_arg(args, int));
-			if (format[i] == 'i')
-				printf("%d", va_arg(args, int));
-			if (format[i] == 'f')
-				printf("%f", va_arg(args, double));
-			if (format[i] == 's')
+			if (format[i] == printers[j].type)
 			{
-				str = va_arg(args, char *);
-				if (str)
-					printf("%s", str);
-				if (!str)
-					printf("(nil)");
+				printf("%s", sep);
+				printers[j].f(args);
+				sep = ", ";
 			}
-			sep = ", ";
-			break;
+			j++;
 		}
 		i++;
 	}
