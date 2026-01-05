097e21182547cbd5e63a3f0e57cd325787c07560
Leyla <Leyla.khaspolad@gmail.com>
2026-01-05 11:03:44 +0000

    Fix 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,78 +3,51 @@
 #include <stdarg.h>
 
 /**
- * print_char - prints a char
- */
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - prints an int
- */
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - prints a float
- */
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - prints a string
- */
-void print_string(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (str)
-		printf("%s", str);
-	if (!str)
-		printf("(nil)");
-}
-
-/**
  * print_all - prints anything
  * @format: format string
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j;
+	unsigned int i = 0;
 	char *sep = "";
-
-	struct printer
-	{
-		char type;
-		void (*f)(va_list);
-	} printers[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
+	char *str;
 
 	va_start(args, format);
 
 	while (format && format[i])
 	{
-		j = 0;
-		while (printers[j].type)
+		switch (format[i])
 		{
-			if (format[i] == printers[j].type)
-			{
-				printf("%s", sep);
-				printers[j].f(args);
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
 				sep = ", ";
-			}
-			j++;
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				sep = ", ";
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				sep = ", ";
+				break;
+			case 's':
+				printf("%s", sep);
+				str = va_arg(args, char *);
+				while (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				while (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				sep = ", ";
+				break;
+			default:
+				break;
 		}
 		i++;
 	}
