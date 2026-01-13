2d984f8d965004108090c90936185cac3699046f
Leyla <Leyla.khaspolad@gmail.com>
2026-01-13 13:07:23 +0000

    Keygen with correct rand sequence

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -10,55 +10,48 @@
  */
 int main(int argc, char *argv[])
 {
-	char key[7];
-	char *username = argv[1];
-	char *lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	int len, i, res;
+	char *u = argv[1];
+	char *l = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	char k[7];
+	int len, i, tmp;
 
 	if (argc != 2)
 		return (1);
 
-	len = strlen(username);
+	len = strlen(u);
 
 	/* 1: Length XOR 59 */
-	key[0] = lookup[(len ^ 59) & 63];
+	k[0] = l[(len ^ 59) & 63];
 
 	/* 2: Sum of chars XOR 79 */
-	res = 0;
-	for (i = 0; i < len; i++)
-		res += username[i];
-	key[1] = lookup[(res ^ 79) & 63];
+	for (i = 0, tmp = 0; i < len; i++)
+		tmp += u[i];
+	k[1] = l[(tmp ^ 79) & 63];
 
 	/* 3: Product of chars XOR 85 */
-	res = 1;
-	for (i = 0; i < len; i++)
-		res *= username[i];
-	key[2] = lookup[(res ^ 85) & 63];
+	for (i = 0, tmp = 1; i < len; i++)
+		tmp *= u[i];
+	k[2] = l[(tmp ^ 85) & 63];
 
-	/* 4: Max char search and srand seed */
-	res = 0;
-	for (i = 0; i < len; i++)
-	{
-		if (username[i] > res)
-			res = username[i];
-	}
-	srand(res ^ 14);
-	key[3] = lookup[rand() & 63];
+	/* 4: Max char search and srand seed (res ^ 14) */
+	for (i = 0, tmp = 0; i < len; i++)
+		if (u[i] > tmp)
+			tmp = u[i];
+	srand(tmp ^ 14);
+	k[3] = l[rand() & 63];
 
 	/* 5: Sum of squares XOR 239 */
-	res = 0;
-	for (i = 0; i < len; i++)
-		res += (username[i] * username[i]);
-	key[4] = lookup[(res ^ 239) & 63];
+	for (i = 0, tmp = 0; i < len; i++)
+		tmp += (u[i] * u[i]);
+	k[4] = l[(tmp ^ 239) & 63];
 
-	/* 6: Random loop based on the FIRST character */
-	res = 0;
-	for (i = 0; i < username[0]; i++)
-		res = rand();
-	key[5] = lookup[(res ^ 229) & 63];
+	/* 6: Random loop based on FIRST char of username */
+	for (i = 0, tmp = 0; i < u[0]; i++)
+		tmp = rand();
+	k[5] = l[(tmp ^ 229) & 63];
 
-	key[6] = '\0';
-	printf("%s", key);
+	k[6] = '\0';
+	printf("%s", k);
 
 	return (0);
 }
