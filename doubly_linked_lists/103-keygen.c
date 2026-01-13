8e6327bf432fd8329b52613fa0fd911fbaf2de44
Leyla <Leyla.khaspolad@gmail.com>
2026-01-13 13:00:53 +0000

    Add keygen for crackme5

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -3,18 +3,17 @@
 #include <string.h>
 
 /**
- * main - Keygen for crackme5
- * @argc: Number of arguments
- * @argv: Argument vector
- *
- * Return: 0 on success, 1 on error
+ * main - Keygen for crackme5.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: 0 on success, 1 on error.
  */
 int main(int argc, char *argv[])
 {
-	char *username;
-	char *lookup = "A-Za-z0-9+/"; /* Common lookup table for this task */
-	int len, i, sum;
 	char key[7];
+	char *username;
+	int len, i, res;
+	char *lookup = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 	if (argc != 2)
 		return (1);
@@ -22,39 +21,36 @@ int main(int argc, char *argv[])
 	username = argv[1];
 	len = strlen(username);
 
-	/* 1st character: Checksum of length */
+	/* Character 1: Based on length */
 	key[0] = lookup[(len ^ 59) & 63];
 
-	/* 2nd character: Sum of ASCII values */
-	sum = 0;
-	for (i = 0; i < len; i++)
-		sum += username[i];
-	key[1] = lookup[(sum ^ 79) & 63];
+	/* Character 2: Sum of ASCII */
+	for (i = 0, res = 0; i < len; i++)
+		res += username[i];
+	key[1] = lookup[(res ^ 79) & 63];
 
-	/* 3rd character: Product of ASCII values */
-	sum = 1;
-	for (i = 0; i < len; i++)
-		sum *= username[i];
-	key[2] = lookup[(sum ^ 85) & 63];
+	/* Character 3: Product of ASCII */
+	for (i = 0, res = 1; i < len; i++)
+		res *= username[i];
+	key[2] = lookup[(res ^ 85) & 63];
 
-	/* 4th character: Highest ASCII value */
-	sum = username[0];
+	/* Character 4: Highest ASCII value */
+	res = 0;
 	for (i = 0; i < len; i++)
-		if (username[i] > sum)
-			sum = username[i];
-	srand(sum ^ 14);
+		if (username[i] > res)
+			res = username[i];
+	srand(res ^ 14);
 	key[3] = lookup[rand() & 63];
 
-	/* 5th character: Sum of squares of ASCII values */
-	sum = 0;
-	for (i = 0; i < len; i++)
-		sum += (username[i] * username[i]);
-	key[4] = lookup[(sum ^ 239) & 63];
+	/* Character 5: Sum of squares */
+	for (i = 0, res = 0; i < len; i++)
+		res += (username[i] * username[i]);
+	key[4] = lookup[(res ^ 239) & 63];
 
-	/* 6th character: Random-based character from first char */
-	for (i = 0, sum = 0; i < username[0]; i++)
-		sum = rand();
-	key[5] = lookup[(sum ^ 229) & 63];
+	/* Character 6: Random based on first character */
+	for (i = 0, res = 0; i < username[0]; i++)
+		res = rand();
+	key[5] = lookup[(res ^ 229) & 63];
 
 	key[6] = '\0';
 	printf("%s", key);
