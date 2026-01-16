786e739045e4ba5627995058a1a9a9054bed77cd
Leyla <Leyla.khaspolad@gmail.com>
2026-01-16 09:42:36 +0000

    Add 103-keygen.c

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,49 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * main - Keygen for crackme5.
- * @argc: Number of arguments.
- * @argv: Argument vector.
- * Return: 0 on success, 1 on failure.
+ * slen - returns length of a C string
+ * @s: string
+ * Return: length
  */
-int main(int argc, char *argv[])
+static int slen(const char *s)
 {
-	char k[7];
-	char *u = argv[1];
-	char *l = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	int len, i, res;
+	int n = 0;
+
+	while (s && s[n])
+	{
+		++n;
+	}
+
+	return (n);
+}
+
+/**
+ * calc_0_2 - calculate idx0, idx1, idx2
+ * @u: username
+ * @len: length of username
+ * @idx0: output
+ * @idx1: output
+ * @idx2: output
+ */
+static void calc_0_2(const char *u, int len, int *idx0, int *idx1, int *idx2)
+{
+	int i;
+	int sum;
+	int prod;
+
+	*idx0 = (len ^ 0x3b) & 0x3f;
+	sum = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		sum += (int)(signed char)u[i];
+	}
+
+	*idx1 = (sum ^ 0x4f) & 0x3f;
+	prod = 1;
+
+	for (i = 0; i < len; i++)
+	{
+		prod *= (int)(signed char)u[i];
+	}
+
+	*idx2 = (prod ^ 0x55) & 0x3f;
+}
+
+/**
+ * calc_3_5 - calculate idx3 and idx5 (uses srand/rand)
+ * @u: username
+ * @len: length of username
+ * @idx3: output
+ * @idx5: output
+ */
+static void calc_3_5(const char *u, int len, int *idx3, int *idx5)
+{
+	int i;
+	int maxc;
+	int r;
+	int n;
+
+	maxc = (int)(signed char)u[0];
+
+	for (i = 0; i < len; i++)
+	{
+		int c = (int)(signed char)u[i];
+
+		if (c > maxc)
+		{
+			maxc = c;
+		}
+	}
+
+	srand((unsigned int)(maxc ^ 0x0e));
+	*idx3 = rand() & 0x3f;
+
+	n = (int)(signed char)u[0];
+	r = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		r = rand();
+	}
+
+	*idx5 = (r ^ 0xe5) & 0x3f;
+}
+
+/**
+ * calc_4 - calculate idx4
+ * @u: username
+ * @len: length of username
+ * Return: idx4
+ */
+static int calc_4(const char *u, int len)
+{
+	int i;
+	int sqsum;
+
+	sqsum = 0;
+
+	for (i = 0; i < len; i++)
+	{
+		int c = (int)(signed char)u[i];
+
+		sqsum += c * c;
+	}
+
+	return ((sqsum ^ 0xef) & 0x3f);
+}
+
+/**
+ * main - keygen for crackme5
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, 1 on usage error
+ */
+int main(int argc, char **argv)
+{
+	const char *u;
+	const char *alpha;
+	int len;
+	int idx0, idx1, idx2, idx3, idx4, idx5;
 
 	if (argc != 2)
-		return (1);
+	{
+		printf("Usage: %s username", argv[0]);
+		exit(1);
+	}
 
-	len = strlen(u);
-	/* Character 1: Length XOR 59 */
-	k[0] = l[(len ^ 59) & 63];
-	/* Character 2: Sum of chars XOR 79 */
-	for (i = 0, res = 0; i < len; i++)
-		res += u[i];
-	k[1] = l[(res ^ 79) & 63];
-	/* Character 3: Product of chars XOR 85 */
-	for (i = 0, res = 1; i < len; i++)
-		res *= u[i];
-	k[2] = l[(res ^ 85) & 63];
-	/* Character 4: Max char search + srand/rand */
-	for (i = 0, res = 0; i < len; i++)
-		if (u[i] > res)
-			res = u[i];
-	srand(res ^ 14);
-	k[3] = l[rand() & 63];
-	/* Character 5: Sum of squares XOR 239 */
-	for (i = 0, res = 0; i < len; i++)
-		res += (u[i] * u[i]);
-	k[4] = l[(res ^ 239) & 63];
-	/* Character 6: Random loop based on first char XOR 229 */
-	for (i = 0, res = 0; i < u[0]; i++)
-		res = rand();
-	k[5] = l[(res ^ 229) & 63];
-	k[6] = '\0';
-	printf("%s", k);
+	u = argv[1];
+	len = slen(u);
+	alpha = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	calc_0_2(u, len, &idx0, &idx1, &idx2);
+	calc_3_5(u, len, &idx3, &idx5);
+	idx4 = calc_4(u, len);
+
+	printf("%c%c%c%c%c%c",
+	       alpha[idx0], alpha[idx1], alpha[idx2],
+	       alpha[idx3], alpha[idx4], alpha[idx5]);
+
 	return (0);
 }
