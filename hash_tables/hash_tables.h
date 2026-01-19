5e4800787cc4e38e7f5a13ea498651f95bee20be
Leyla <Leyla.khaspolad@gmail.com>
2026-01-19 10:18:02 +0000

    Add 1-djb2.c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -29,6 +29,27 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+/* --- Function Prototypes --- */
+
+/* Task 0: Create hash table */
 hash_table_t *hash_table_create(unsigned long int size);
+
+/* Task 1: djb2 algorithm */
+unsigned long int hash_djb2(const unsigned char *str);
+
+/* Task 2: Key index */
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+
+/* Task 3: Set/Add to hash table */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+/* Task 4: Get value from key */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
+/* Task 5: Print hash table */
+void hash_table_print(const hash_table_t *ht);
+
+/* Task 6: Delete hash table */
+void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
