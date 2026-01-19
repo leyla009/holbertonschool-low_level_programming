5f3497b973027b1af1e0592f7e12fd5f218c1505
Leyla <Leyla.khaspolad@gmail.com>
2026-01-19 13:51:51 +0000

    Fix Betty Stle coding issues

---
+++ file_io/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/* Task 0 */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/* Task 1 */
+int create_file(const char *filename, char *text_content);
+
+/* Task 2 */
+int append_text_to_file(const char *filename, char *text_content);
+
+/* Task 4 (Advanced) */
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+
+#endif /* MAIN_H */
