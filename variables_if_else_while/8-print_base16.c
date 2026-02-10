0e44b08f13745c70c204a080461ea48164a9b6e1
leyla009 <89606168+leyla009@users.noreply.github.com>
2026-02-10 16:50:21 +0400

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,11 +10,15 @@ int main(void)
 	int n;
 	char c;
 
-	for (n = 0; n < 10; n++)
-		putchar(n + '0');
+	for (n = 0; n <= 9; n++) 
+		{
+			putchar(n);
+		}
 
 	for (c = 'a'; c <= 'f'; c++)
-		putchar(c);
+		{
+			putchar(c);
+		}
 
 	putchar('\n');
 
